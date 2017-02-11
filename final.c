3f595ae30bfa9186f38e037598b44e35dae64f6a
Bonnie <kbonnie@gmail.com>
2017-02-10 17:54:38 -0800

    working on structs

--- final.c
+++ final.c
@@ -48,7 +48,7 @@ void openDirCreateStructs(){
       // if the file is  . or .. file, break out of while loop
       printf("this is a . file\n"); // actually should be break here
     } else {
-      //printf("Files are %s\n", sd->d_name);
+      // here is looping over files
 
       // use a varialbe fileName to store the file name
       char fileName[10];
@@ -63,15 +63,38 @@ void openDirCreateStructs(){
       printf("%s\n", filePath);
 
       // set the file pointer to the correct name
-      //FILE *roomFile = fopen("./kwongb.rooms/ALPHA", "r");
+      FILE *roomFile = fopen(filePath, "r");
+
+      // optional, but check if file pointer is null
+      if(roomFile == NULL){
+        printf("roomFile is  null");
+      }
 
       // now that we have a file, let's read the first line and get the room name
       char line[50];
+
+      // create a struct with the name of room name. Actual struct name could be anything
+      struct Room baz;
+      baz.roomName = fileName;
+
+      while(fgets(line, sizeof(line), roomFile) != NULL){
+         printf("Created a struct with room name: %s\n", baz.roomName);
+
+      }
+
+      // now that struct is complete, put it into struct array
+      rooms7[structCounter] = baz;
+      structCounter++;
+
+      // close roomFile pointer
+      fclose(roomFile);
 
 
     }
 
   }
+
+  printf("the last struct is %s\n", rooms7[3].roomName);
 
   // close directory
   closedir(dir);
